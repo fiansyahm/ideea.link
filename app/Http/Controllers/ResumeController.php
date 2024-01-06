a679ddf6f1c2715b2ebe211c3cc5f0bfe1a03a5b
fiansyahm <fiansyahmm@gmail.com>
2024-01-06 17:38:27 +0700

    update resume

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -21,9 +21,83 @@ class ResumeController extends Controller
 
      public function personalProfile($id)
      {
+        $canido = '{
+            "data": [
+              {
+                "name": "Graphic Design",
+                "icon": "icon flaticon-layers-icon"
+              },
+              {
+                "name": "Web Design",
+                "icon": "icon flaticon-responsive-design-symbol"
+              },
+              {
+                "name": "Photography",
+                "icon": "icon flaticon-technology"
+              }
+            ]
+          }';
+
+          $skill = '{
+            "data": [
+              {
+                "name": "HTML",
+                "percentage": "92"
+              },
+              {
+                "name": "CSS",
+                "percentage": "60"
+              },
+              {
+                "name": "JS",
+                "percentage": "20"
+              }
+            ]
+          }';
+
+          $educational = '{
+            "data": [
+              {
+                "degree": "Master Degree",
+                "university": "University of Oxford",
+                "year": "Jan 2015 - Dec 2017",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus."
+              },
+                {
+                    "degree": "Bachelor Degree",
+                    "university": "University of Oxford",
+                    "year": "Jan 2015 - Dec 2017",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus."
+                }
+            ]
+          }';
+
+          $working = '{
+            "data": [
+              {
+                "position": "UI/UX",
+                "company": "University of Oxford",
+                "year": "Jan 2015 - Dec 2017",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus."
+              },
+                {
+                    "position": "Front End Developer",
+                    "company": "University of Oxford",
+                    "year": "Jan 2015 - Dec 2017",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus."
+                }
+            ]
+          }';
+
          $resume = Resume::where('id', $id)
                          ->orWhere('name', $id)
                          ->first();
+
+        // $resume->canido = $canido;
+        // $resume->skill = $skill;
+        // $resume->educational = $educational;
+        $resume->working = $working;
+        $resume->save();
  
          if ($resume) {
              $view = 'resume.theme.theme-' . $resume->theme;
--- resources/views/resume/theme/theme-1.blade.php
+++ resources/views/resume/theme/theme-1.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentleman</title>
+    <title>{{$resume->name}} Resume</title>
 
 
     <!-- Fonts -->
@@ -56,7 +56,7 @@
           <span class="icon-bar third_icon"></span>
         </button>
         <div class="header_image">
-          <a href="index.html"><img src="{{ asset('theme/gentleman/assets/images/about.jpg')}}" alt="Logo" class="img-responsive"></a>
+          <a href="index.html"><img src="{{$resume->photo}}" alt="Logo" class="img-responsive"></a>
         </div>
 
         <ul class="left_menu nav">
@@ -170,13 +170,25 @@
           <div class="banner_content">
             <h4>Hi There! I am</h4>
             <h2>{{$resume->name}}</h2>
-            <p>Professional Graphic Designer</p>
+            <p>{{$resume->job}}</p>
             <ul class="banner_social">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+              @if($resume->linkedin != null)
+              <li><a href="{{$resume->linkedin}}"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+              @endif
+              @if($resume->instagram != null)
+              <li><a href="{{$resume->instagram}}"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              @endif
+              @if($resume->facebook != null)
+              <li><a href="{{$resume->facebook}}"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              @endif
+              @if($resume->twitter != null)
+              <li><a href="{{$resume->twitter}}"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+              @endif
+              @if($resume->github != null)
+              <li><a href="{{$resume->github}}"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+              @endif
+              {{-- <li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li> --}}
             </ul>
           </div><!--banner content-->
         </div><!--body_container-->
@@ -196,67 +208,66 @@
   help you for our Friendshiop. </em></p>
               </div><!-- section header -->
               <div class="about_details">
-                <p>Unique utilize plug-and-play best practices vis-a-vis out ofbox strategic theme area. Efficient enga backward-compatible collaboration and idea-share before our client based functionalities. Rapidiously visualize world-class architectures with mission critical intellectual capital.</p>
-                <p>Progressively myocardinate state of the art ROI. Monotonectally disseminate unique methods of empowerment.</p>
+                <p>{{$resume->aboutme}}</p>
+                {{-- <p>Progressively myocardinate state of the art ROI. Monotonectally disseminate unique methods of empowerment.</p> --}}
                 <ul>
-                  <li><p>Name<span>:</span></p> <span>Alex Mohon</span></li>
-                  <li><p>Phone<span>:</span></p> <span>+880 1234 567890, +880 9876 543210</span></li>
-                  <li><p>E-mail<span>:</span></p> <span>contact@yourmail.com</span></li>
-                  <li><p>Address<span>:</span></p> <span>Streop Rd, Peosur, Inphodux, USA.</span></li>
-                  <li><p>Date of Birth<span>:</span></p> <span>09 July 1984</span></li>
-                  <li><p>Website<span>:</span></p> <span>www.yourmail.com</span></li>
-                  <li><p>Freelance<span>:</span></p> <span>Available</span></li>
+                  <li><p>Name<span>:</span></p> <span>{{$resume->name}}</span></li>
+                  @if($resume->phone != null)
+                  <li><p>Phone<span>:</span></p> <span>{{$resume->phone}}</span></li>
+                  @endif
+                  @if($resume->email != null)
+                  <li><p>E-mail<span>:</span></p> <span> {{$resume->email}}</span></li>
+                  @endif
+                  @if($resume->address != null)
+                  <li><p>Address<span>:</span></p> <span>{{$resume->address}}</span></li>
+                  @endif
+                  @if($resume->birth != null)
+                  <li><p>Date of Birth<span>:</span></p> <span>{{$resume->birth}}</span></li>
+                  @endif
+                  @if($resume->website != null)
+                  <li><p>Website<span>:</span></p> <span>{{$resume->website}}</span></li>
+                  @endif
+                  @if($resume->freelance != null)
+                  <li><p>Freelance<span>:</span></p> <span>{{$resume->freelance}}</span></li>
+                  @endif
                 </ul>
               </div><!-- about details -->
             </div><!-- column -->
             <div class="col-md-6 col-sm-12 col-xs-12">
               <div class="about_job">
                 <h3>What can i Do</h3>
+                <?php
+                  // Konversi string JSON ke dalam bentuk array menggunakan json_decode
+                  $canido = json_decode($resume->canido, true);
+                ?>
                 <ul>
-                  <li><span class="icon flaticon-layers-icon"></span><p>Graphic Design</p></li>
-                  <li><span class="icon flaticon-responsive-design-symbol"></span><p>Web Design</p></li>
-                  <li><span class="icon flaticon-technology"></span><p>Photography</p></li>
+                  @foreach ($canido['data'] as $item)
+                    <li>
+                      <span class="{{ $item['icon'] }}"></span>
+                      <p>{{ $item['name'] }}</p>
+                    </li>
+                  @endforeach
                 </ul>
               </div><!-- about job -->
 
               <div class="about_skills">
                 <h3>My Skills</h3>
                 <div class="skills_progress">
+                  <?php
+                    // Konversi string JSON ke dalam bentuk array menggunakan json_decode
+                    $skill = json_decode($resume->skill, true);
+                  ?>
+                  @foreach ($skill['data'] as $item)
                   <div class="progress_item">
-                    <p class="progress_title">Photoshop</p>
+                    <p class="progress_title">{{ $item['name'] }}</p>
                     <div class="progress">
-                      <div class="progress-bar" role="progressbar" data-transitiongoal="92"><span class="pg_indicator"></span>
+                      <div class="progress-bar" role="progressbar" data-transitiongoal="{{ $item['percentage'] }}"><span class="pg_indicator"></span>
                       </div>
                     </div>
-                    <p class="progress_percent">92%</p>
+                    <p class="progress_percent">{{ $item['percentage'] }}</p>
                   </div>
+                  @endforeach
 
-                  <div class="progress_item">
-                    <p class="progress_title">Illustrator</p>
-                    <div class="progress">
-                      <div class="progress-bar" role="progressbar" data-transitiongoal="86"><span class="pg_indicator"></span>
-                      </div>
-                    </div>
-                    <p class="progress_percent">86%</p>
-                  </div>
-
-                  <div class="progress_item">
-                    <p class="progress_title">Wn Design</p>
-                    <div class="progress">
-                      <div class="progress-bar" role="progressbar" data-transitiongoal="42"><span class="pg_indicator"></span>
-                      </div>
-                    </div>
-                    <p class="progress_percent">42%</p>
-                  </div>
-
-                  <div class="progress_item">
-                    <p class="progress_title">Corel Drow</p>
-                    <div class="progress">
-                      <div class="progress-bar" role="progressbar" data-transitiongoal="60"><span class="pg_indicator"></span>
-                      </div>
-                    </div>
-                    <p class="progress_percent">60%</p>
-                  </div>
                 </div><!-- skills progress -->
               </div><!-- about skills -->
             </div><!-- column -->
@@ -282,29 +293,19 @@
                 <h3>Educational Experience</h3>
                 <div class="experiences left">
 
-                  <div class="experience">
-                    <h4>Master Degree</h4>
-                    <p>University of Oxford / <span>Jan 2015 - Dec 2017</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
-                  </div><!-- experience -->
+                  
 
+                <?php
+                    // Konversi string JSON ke dalam bentuk array menggunakan json_decode
+                    $educational = json_decode($resume->educational, true);
+                ?>
+                  @foreach ($educational['data'] as $item)
                   <div class="experience">
-                    <h4>Bachelors Degree</h4>
-                    <p>University of California / <span>Jan 2011 - Dec 2015</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
+                    <h4>{{ $item['degree'] }}</h4>
+                    <p>{{ $item['university'] }} / <span>{{ $item['year'] }}</span></p>
+                    <p class="ex_details">{{ $item['description'] }}.</p>
                   </div><!-- experience -->
-
-                  <div class="experience">
-                    <h4>Deploma Engineering</h4>
-                    <p>Stanford University / <span>Jan 2008 - Dec 2011</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
-                  </div><!-- experience -->
-
-                  <div class="experience">
-                    <h4>Higher Secondary</h4>
-                    <p>Stanford University / <span>Jan 2006 - Dec 2008</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
-                  </div><!-- experience -->
+                  @endforeach
 
                 </div><!-- experience -->
               </div>
@@ -312,29 +313,18 @@
                 <h3>Working Experience</h3>
                 <div class="experiences right">
 
-                  <div class="experience">
-                    <h4>Lead UI/UX Designer</h4>
-                    <p>CodexCoder / <span>Jan 2014 - Dec 2017</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
-                  </div><!-- experience -->
+                  <?php
+                      // Konversi string JSON ke dalam bentuk array menggunakan json_decode
+                      $working = json_decode($resume->working, true);
+                  ?>
 
+                  @foreach ($working['data'] as $item)
                   <div class="experience">
-                    <h4>UI/UX Designer</h4>
-                    <p>Webcode Ltd. / <span>Jan 2012 - Dec 2014</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
+                    <h4>{{ $item['position'] }}</h4>
+                    <p>{{ $item['company'] }} / <span>{{ $item['year'] }}</span></p>
+                    <p class="ex_details">{{ $item['description'] }}.</p>
                   </div><!-- experience -->
-
-                  <div class="experience">
-                    <h4>Senior Graphic Designer</h4>
-                    <p>Webcode Ltd. / <span>Jan 2012 - May 2012</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
-                  </div><!-- experience -->
-
-                  <div class="experience">
-                    <h4>Graphic Designer</h4>
-                    <p>Webcode Ltd. / <span>Jan 2011 - Dec 2012</span></p>
-                    <p class="ex_details">Uniquely incentivize process-centric systems for reliable market. Authoritatively scale next generation collaboration. Globally morph 24/365 e-commerce.</p>
-                  </div><!-- experience -->
+                  @endforeach
 
                 </div><!-- experience -->
               </div>
