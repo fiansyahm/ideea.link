bf16f26717aa418a12c84fa7cca81e239d9ea37b
Tria Elvafizani <triaelva@gmail.com>
2024-01-12 13:26:09 +0700

    update

--- resources/views/services/android.blade.php
+++ resources/views/services/android.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Android')
 @section('main')
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
     <html lang="en">
 
     <head>
@@ -21,30 +21,22 @@
                     <div class="col-md">
                         <div class="mt-5 container">
                             <div>
-                                <h1 class="mt-5"
+                                <h1 class="mt-5 mb-5"
                                     style="font-family: 'Poppins', sans-serif; font-size: 42px; color: #000000; font-weight: bold;">
                                     Menghadirkan Mobile Apps Yang Mudah Digunakan</h1>
-                                <p class="space-between"
-                                    style="padding-inline-end: 100px; font-family: 'Poppins', sans-serif; font-size: 20px;">
-                                    Kami menggunakan UI/UX design practices untuk membuat desain aplikasi dan website yang
-                                    baik
-                                    sesuai dengan kebutuhan user Anda.
-                                </p>
                                 <button type="button" class="btn btn-primary btn-lg">Konsultasi GRATIS</button>
                             </div>
                         </div>
-
                     </div>
-                    {{-- Gambar animasi UI/UX --}}
-                    <div class="col-md">
-                        <img class="img-fluid mt-4 pt-4" src="https://i.ibb.co/9HfZrwY/uiux.png">
+                    {{-- Gambar animasi Android --}}
+                    <div class="col-md mb-5">
+                        <img class="img-fluid mt-4 pt-4" src="https://i.ibb.co/vDzn8H4/android.png">
                     </div>
-
                 </div>
             </div>
         </div>
 
-   </body>
+    </body>
 
-{{-- https://www.jmc.co.id/mobile-apps --}}
+    {{-- https://www.jmc.co.id/mobile-apps --}}
 @endsection
